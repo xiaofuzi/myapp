107e12d43a4cb8d23d08da638c913f194af26501
xiaofuzhi <xiaofuzhi@gmail.com>
2013-04-20 13:22:38 +0800

    viewcommit

--- app/helpers/staticpages_helper.rb
+++ app/helpers/staticpages_helper.rb
@@ -1,2 +1,12 @@
 module StaticpagesHelper
+
+# 根据所在页面返回完整的标题                           # 在文档中显示的注释
+  def full_title(page_title)                          # 方法定义
+    base_title = "Fenxue Sample App"  # 变量赋值
+    if page_title.empty?                              # 布尔测试
+      base_title                                      # 非显式返回值
+    else
+      "#{base_title} | #{page_title}"                 # 字符串插值
+    end
+  end
 end
